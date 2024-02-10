dba31a888ee9aa4697db6cf9c2e6d1f014494643
AllunDev <allun.development@gmail.com>
2024-02-10 15:12:56 +0100

    Fixed scrap collected trigger

--- RumblingCompany/Patches/RoundManager.cs
+++ RumblingCompany/Patches/RoundManager.cs
@@ -6,9 +6,12 @@ namespace RumblingCompany.Patches
     internal class RoundManagerPatch
     {
         [HarmonyPatch(typeof(RoundManager), "CollectNewScrapForThisRound")]
-        [HarmonyPostfix]
-        private static void CollectNewScrapForThisRoundPatch(){
+        [HarmonyPrefix]
+        private static void CollectNewScrapForThisRoundPatch(GrabbableObject scrapObject){
+            if (scrapObject.playerHeldBy != GameNetworkManager.Instance.localPlayerController) return;
             
+            if (RoundManager.Instance.scrapCollectedThisRound.Contains(scrapObject) || scrapObject.scrapPersistedThroughRounds) return;
+
             Plugin.Mls.LogInfo($"Client collected scrap, vibrating");
             Plugin.DeviceManager.increaseVibration(0.25f);
         }
